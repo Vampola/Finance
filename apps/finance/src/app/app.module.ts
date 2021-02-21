ff60f41c505842f7f83d64346787052f94d52804
Damir <vampola.damir@gmail.com>
2021-02-22 00:22:31 +0100

    Added routes

--- apps/finance/src/app/app.module.ts
+++ apps/finance/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ExpencesComponent } from './expences/expences.component';
+
+const appRoutes: Routes = [
+  { path: 'expences', component: ExpencesComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ExpencesComponent],
+  imports: [
+    BrowserModule, 
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/finance/src/app/expences/expences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'finance-expences',
+  templateUrl: './expences.component.html',
+  styleUrls: ['./expences.component.sass']
+})
+export class ExpencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
